b1d90f6962ae5274bb7118f5ab40270820d60969
kousei <kottus629@outlook.jp>
2026-02-10 21:17:49 +0900

    modelsディレクトリ

--- backend/models/bid.go
+++ backend/models/bid.go
@@ -19,19 +19,3 @@ type Bid struct {
 	RequestID string    `json:"request_id"`
 	CreatedAt time.Time `json:"created_at"`
 }
-
-// POST /cars
-type CarCreateRequest struct {
-	Model string `json:"model"`
-	Price int    `json:"price"`
-	Year  int    `json:"year"`
-}
-
-// GET /cars item
-type Car struct {
-	ID        int64     `json:"id"`
-	Model     string    `json:"model"`
-	Price     int       `json:"price"`
-	Year      int       `json:"year"`
-	CreatedAt time.Time `json:"created_at"`
-}
