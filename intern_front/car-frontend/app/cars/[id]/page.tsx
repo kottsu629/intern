cc70bf7d4eda710672a50e8b0e015eff68548ed2
kousei <kottus629@outlook.jp>
2026-02-16 16:51:16 +0900

    any修正

--- intern_front/car-frontend/app/cars/[id]/page.tsx
+++ intern_front/car-frontend/app/cars/[id]/page.tsx
@@ -1,6 +1,12 @@
 import { CarDetailContainer } from './CarDetailContainer';
 
-export default async function Page({ params }: any) {
+type PageProps = {
+  params: {
+    id: string;
+  };
+};
+
+export default async function Page({ params }: PageProps) {
   const { id } = await Promise.resolve(params);
   return <CarDetailContainer id={id} />;
 }
