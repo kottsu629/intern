3d7e6389688128ceadd649a40dfcc29b7beb3105
kousei <kottus629@outlook.jp>
2026-03-02 15:03:51 +0900

    fetchの移動

--- intern_front/car-frontend/app/cars/[id]/_components/car-detail/CarDetailContainer.tsx
+++ intern_front/car-frontend/app/cars/[id]/_components/car-detail/CarDetailContainer.tsx
@@ -1,10 +1,23 @@
 import type { Car } from '../../types';
 import { CarDetailPresentation } from './CarDetailPresentation';
+import { notFound } from 'next/navigation';
+import { API_BASE } from '../../_lib/api';
 
-export function CarDetailContainer(props: { id: string; initialCar: Car }) {
-  const { id, initialCar } = props;
-
+export async function CarDetailContainer(props: { id: string }) {
+  const { id } = props;
   const carId = Number(id);
+  if (Number.isNaN(carId)) notFound();
 
-  return <CarDetailPresentation carId={carId} car={initialCar} />;
+  async function fetchCarOr404(idNum: number): Promise<Car> {
+    const res = await fetch(`${API_BASE}/cars/${idNum}`, { cache: 'no-store' });
+
+    if (res.status === 404) notFound();
+    if (!res.ok) throw new Error('車両取得に失敗しました。');
+
+    return res.json();
+  }
+
+  const car = await fetchCarOr404(carId);
+
+  return <CarDetailPresentation carId={carId} car={car} />;
 }
--- intern_front/car-frontend/app/cars/[id]/page.tsx
+++ intern_front/car-frontend/app/cars/[id]/page.tsx
@@ -1,28 +1,11 @@
-import { notFound } from 'next/navigation';
 import { CarDetailContainer } from './_components/car-detail/CarDetailContainer';
-import { API_BASE } from './_lib/api';
-import type { Car } from './types';
 
 type PageProps = {
   params: { id: string };
 };
 
-async function fetchCarOr404(idNum: number): Promise<Car> {
-  
-  const res = await fetch(`${API_BASE}/cars/${idNum}`, { cache: 'no-store' });
-
-  if (res.status === 404) notFound();
-  if (!res.ok) throw new Error('車両取得に失敗しました。');
-
-  return res.json();
-}
-
 export default async function Page({ params }: PageProps) {
   const { id } = await params;
 
-  const idNum = Number(id);
-  if (Number.isNaN(idNum)) notFound();
-
-  const car = await fetchCarOr404(idNum);
-  return <CarDetailContainer id={id} initialCar={car} />;
+  return <CarDetailContainer id={id}  />;
 }
